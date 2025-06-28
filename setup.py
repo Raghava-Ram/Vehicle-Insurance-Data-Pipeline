a35c19df4132c151ade3a6995b0b06c42c3359b6
RaghavaRam <raghavaram219@gmail.com>
2025-06-28 19:09:45 +0530

    project structure ready and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="RaghavaRam",
+    author_email="raghavaram219@gmail.com",
+    packages=find_packages()
+)
